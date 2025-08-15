89f05897db96390d37b0242cd598e00f088c6d4c
Jochen-code <vchen338@gmail.com>
2025-08-15 08:56:09 +0800

    Add files via upload

---
+++ Basic Calculator C#.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Basic_Calculator
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter 1st Number: ");
+			int num1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Enter 2nd Number: ");
+			int num2 = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("Select Operator \n1. + \n2. -");
+			Console.Write("?: ");
+			int input = Convert.ToInt32(Console.ReadLine());
+			
+			switch (input)
+			{
+			
+				case 1:
+					Console.Write("Result " + (num1 + num2));
+					break;
+					
+				case 2:
+					Console.Write("Result " + (num1 - num2));
+					break;
+				
+				default:
+                Console.WriteLine("Invalid choice.");
+                break;
+			}
+			
+		}
+	}
+
+}
