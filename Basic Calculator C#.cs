70868662658771974ec5aba6c4236e3ab62bcd55
Jochen-code <vchen338@gmail.com>
2025-08-15 09:03:36 +0800

    Update Basic Calculator C#.cs

--- Basic Calculator C#.cs
+++ Basic Calculator C#.cs
@@ -1,37 +1,76 @@
-﻿using System;
+using System;
 
 namespace Basic_Calculator
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.Write("Enter 1st Number: ");
-			int num1 = Convert.ToInt32(Console.ReadLine());
-			Console.Write("Enter 2nd Number: ");
-			int num2 = Convert.ToInt32(Console.ReadLine());
-			
-			Console.WriteLine("Select Operator \n1. + \n2. -");
-			Console.Write("?: ");
-			int input = Convert.ToInt32(Console.ReadLine());
-			
-			switch (input)
-			{
-			
-				case 1:
-					Console.Write("Result " + (num1 + num2));
-					break;
-					
-				case 2:
-					Console.Write("Result " + (num1 - num2));
-					break;
-				
-				default:
-                Console.WriteLine("Invalid choice.");
-                break;
-			}
-			
-		}
-	}
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter 1st Number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
 
+                Console.Write("Enter 2nd Number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Select Operator (+, -, *, /, %, ^)");
+                Console.Write("?: ");
+                string op = Console.ReadLine();
+
+                double result;
+
+                switch (op)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    case "/":
+                        if (num2 == 0)
+                            Console.WriteLine("Error: Division by zero is not allowed.");
+                        else
+                        {
+                            result = num1 / num2;
+                            Console.WriteLine("Result: " + result);
+                        }
+                        break;
+
+                    case "%":
+                        if (num2 == 0)
+                            Console.WriteLine("Error: Modulus by zero is not allowed.");
+                        else
+                        {
+                            result = num1 % num2;
+                            Console.WriteLine("Result: " + result);
+                        }
+                        break;
+
+                    case "^":
+                        result = Math.Pow(num1, num2);
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid operator.");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Please enter valid numbers.");
+            }
+        }
+    }
 }
